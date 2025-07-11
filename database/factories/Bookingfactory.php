f902e2ed65ece83a647b766106a730f85d55da3f
Alex Pasquero <axelpasquero@gmail.com>
2025-07-11 18:52:34 +0200

    edit comment

--- database/factories/Bookingfactory.php
+++ database/factories/Bookingfactory.php
@@ -19,22 +19,22 @@ class BookingFactory extends Factory
      */
     public function definition(): array
     {
-        // Prendi un utente e un campo a caso dal database
+        
         $user = User::inRandomOrder()->first() ?? User::factory()->create();
         $field = Field::inRandomOrder()->first() ?? Field::factory()->create();
 
-        // Genera una data di prenotazione casuale (da 1 mese fa a 1 mese nel futuro)
+         
         $bookingDate = $this->faker->dateTimeBetween('-1 month', '+1 month');
         
-        // Genera un orario di inizio casuale tra le 8:00 e le 20:00
+        
         $startHour = $this->faker->numberBetween(8, 20);
         $startTime = Carbon::instance($bookingDate)->setTime($startHour, 0, 0);
 
-        // Imposta una durata di 1 o 2 ore
+       
         $durationInHours = $this->faker->randomElement([1, 2]);
         $endTime = $startTime->copy()->addHours($durationInHours);
 
-        // Calcola il prezzo totale
+        
         $totalPrice = $field->price_per_hour * $durationInHours;
 
         return [
